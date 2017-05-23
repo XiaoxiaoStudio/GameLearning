4db6481c0542a43cd9aa136b89aeabf608ff7dec
DoubleBo <1226410762@qq.com>
2017-05-23 20:27:47 +0800

    SaveLogFiles(can trun off print log)

---
+++ DoubleBo/PrintAndSavaLogInfo/Assets/PrintLog/PrintDebug/PrintDebug/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PrintDebug
+{
+    public class DebugLog
+    {
+        /// <summary>
+        /// 定义一个标志 用于控制是否输出日志
+        /// </summary>
+        static public bool EnableLog = true;
+
+        /// <summary>
+        /// 打印日志 但是没有传递打印日志对应的组件
+        /// </summary>
+        /// <param name="message"></param>
+        static public void Log(object message)
+        {
+            Log(message, null);
+        }
+
+        /// <summary>
+        /// 打印日志 并显示打印日志对应的组件
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="context"></param>
+        static public void Log(object message, Object context)
+        {
+            if (EnableLog)
+            {
+                ////message为打印的信息，context为打印信息对应的组件
+                Debug.Log(message, context);
+            }
+        }
+
+        static public void LogError(object message)
+        {
+            LogError(message, null);
+        }
+        static public void LogError(object message, Object context)
+        {
+            if (EnableLog)
+            {
+                Debug.LogError(message, context);
+            }
+        }
+
+        static public void LogWarning(object message)
+        {
+            LogWarning(message, null);
+        }
+        static public void LogWarning(object message, Object context)
+        {
+            if (EnableLog)
+            {
+                Debug.LogWarning(message, context);
+            }
+        }
+    }
+}
